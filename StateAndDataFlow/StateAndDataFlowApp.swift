97699d39f69376ce1f8c8da1c8be1b8094b6f0c0
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-05-20 19:46:17 +0700

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Алексей Синяговский on 20.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
